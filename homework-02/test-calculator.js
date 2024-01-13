a5fd258579b450dfc071cf9c524c4505f8c0d259
v.kokoiev <v.kokoiev@steel.mil.ua>
2024-01-13 17:17:33 +0200

    my commit npm package

---
+++ homework-02/test-calculator.js
@@ -0,0 +1,10 @@
+const calculator = require('my-calculator-v01');
+console.log(calculator.div(3,0));
+console.log(calculator.add(1, 15));
+console.log(calculator.sub(6,7));
+console.log(calculator.add(1, 5));
+console.log(calculator.div(2, 1));
+console.log(calculator.sub(10, 3));
+console.log(calculator.mul(3,3));
+console.log(calculator.div(3,0));
+
